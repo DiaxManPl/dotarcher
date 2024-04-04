4ffac82ea06491c3666ddbbe5dcb1bca2ae26aff
DiaxManPl <diaxman.gameplay@gmail.com>
2024-04-04 11:29:32 +0200

    Update

--- wlogout/launch.sh
+++ wlogout/launch.sh
@@ -6,26 +6,21 @@ then
     exit 0
 fi
 
-layout="./layout"
-template="./style.css"
+w_path=`dirname "$(realpath $0)"`
+
+template="$w_path/style.css"
 
 monitor_w=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
 monitor_h=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height')
 monitor_scale=$(hyprctl -j monitors | jq '.[] | select (.focused == true) | .scale' | sed 's/\.//')
 hypr_border=`hyprctl -j getoption decoration:rounding | jq '.int'`
 
-export x_mgn=$(( monitor_w * 35 / monitor_scale ))
-export y_mgn=$(( monitor_h * 25 / monitor_scale ))
-export x_hvr=$(( monitor_w * 32 / monitor_scale ))
-export y_hvr=$(( monitor_h * 20 / monitor_scale ))
+export margin_x=$(( monitor_w * 35 / monitor_scale ))
+export margin_y=$(( monitor_h * 25 / monitor_scale ))
 
-export fontSize=$(( monitor_h * 2 / 100 ))
-
+export font_size=$(( monitor_h * 2 / 100 ))
 export active_radius=$(( hypr_border * 5 ))
 export button_radius=$(( hypr_border * 8 ))
-export button_background="#222"
-export button_color="#ff0000"
 
 style=`envsubst < $template`
-echo $style
 wlogout -b 2 -c 0 -r 0 -m 0 --css <(echo "$style")
